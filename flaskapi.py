42f2f46f3c3e13a33899351823f18a583f5aec9f
Jack Whitehouse <jackwhitehouse@pc-78-182.customer.ask4.lan>
2023-06-11 17:28:24 +0100

    fifth commit

--- flaskapi.py
+++ flaskapi.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/test')
 def welcome():
-    return "true"
+    test = "true"
+    res = json.dumps(test)
+    return res
 
 if __name__ == '__main__':
     app.run()
