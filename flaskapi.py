2903bcb8d68ec69adb70c588faee020856b9c503
Jack Whitehouse <jackwhitehouse@pc-78-182.customer.ask4.lan>
2023-06-11 20:10:34 +0100

    sixth commit

--- flaskapi.py
+++ flaskapi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 
 app = Flask(__name__)
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/test')
 def welcome():
     test = "true"
-    res = json.dumps(test)
-    return res
+    return jsonify({'bool': test})
+
 
 if __name__ == '__main__':
     app.run()
