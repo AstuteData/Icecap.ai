05bca68c982a8f1ad9ac4cec44cffa217400da67
Jack Whitehouse <jackwhitehouse@pc-78-182.customer.ask4.lan>
2023-07-02 16:13:42 +0100

    func test

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ import celery
 import articleResearch
 
 app = celery.Celery('example')
-app.conf.update(BROKER_URL='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-126-144.eu-west-1.compute.amazonaws.com:16830')
+app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-126-144.eu-west-1.compute.amazonaws.com:16830')
 
 
 @app.task
