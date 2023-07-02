091e30cd436e055aeb8846c6ef540ae875cc4219
Jack Whitehouse <jackwhitehouse@pc-78-182.customer.ask4.lan>
2023-07-02 13:28:32 +0100

    func test

--- task.py
+++ task.py
@@ -5,7 +5,7 @@ import articleResearch
 app1 = celery.Celery('example')
 app1.conf.update(BROKER_URL=os.environ['rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-126-144.eu-west-1.compute.amazonaws.com:16830'])
 
-@app.task
+@app1.task
 def researchworker(jsonstring):
     articleResearch.prep_article_data(jsonstring)
     return "complete"
