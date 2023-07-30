c2f49fe4cfcdd5310883ca066c804f6a40a41a1b
Jack Whitehouse <jackwhitehouse@jacks-mbp.home>
2023-07-30 17:17:04 +0100

    func test

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import articleResearch
 import time
 
 app = celery.Celery('example')
-app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-126-144.eu-west-1.compute.amazonaws.com:16830')
+app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-52-31-163-223.eu-west-1.compute.amazonaws.com:18690')
 
 @app.task
 def researchworker(jsonstring):
