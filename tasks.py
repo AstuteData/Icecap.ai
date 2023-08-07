c8a639f28c541a4c011472d2c7f2743abc442df2
AstuteData <jack@jackwhitehouse.co.uk>
2023-08-07 12:09:25 +0100

    func test

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import articleResearch
 import time
 
 app = celery.Celery('example')
-app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-52-31-163-223.eu-west-1.compute.amazonaws.com:18690')
+app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-43-20.eu-west-1.compute.amazonaws.com:21080')
 
 @app.task
 def researchworker(jsonstring):
