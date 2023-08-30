a6919388765f6d381a3f11c97d64af8e36bb0ba0
AstuteData <jack@jackwhitehouse.co.uk>
2023-08-30 23:11:53 +0100

    func test

--- scrapers/uploadscraper.py
+++ scrapers/uploadscraper.py
@@ -27,10 +27,9 @@ except Exception as error:
 
 def start_research(upload_data):
     # Organising the json from upload_data
-    upload_data_list = json.loads(upload_data)
-    domain_header = upload_data_list['website header']
-    linkedin_profile_header = upload_data_list['linkedin profile header']
-    csv_data = pd.DataFrame.from_dict(upload_data_list['csv'])
+    domain_header = upload_data['website header']
+    linkedin_profile_header = upload_data['linkedin profile header']
+    csv_data = pd.DataFrame.from_dict(upload_data['csv'])
     companies_to_research = []
 
     # Researching the company and prospect starts here
