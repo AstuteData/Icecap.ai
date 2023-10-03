e8613f54c70d19654aace0555fb3d27fe6d799f2
AstuteData <jack@jackwhitehouse.co.uk>
2023-10-03 19:21:55 +0100

    Icecap MVP Version 1.0

--- tasks.py
+++ tasks.py
@@ -1,11 +1,12 @@
 import celery
-from scrapers import uploadscraper, articlescraper
-import time
+import sys
+sys.path.append('../')
+from main.core_platform.platform_main import research_main
 
 app = celery.Celery('example')
 app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-43-20.eu-west-1.compute.amazonaws.com:21080')
 
 @app.task
 def researchworker(upload_data):
-    uploadscraper.start_research(upload_data)
+    response = research_main.format_csv(upload_data)
     return "Scraping complete"
