9b46d4222f77a92ab42ecfeff67e011b561083dd
AstuteData <jack@jackwhitehouse.co.uk>
2023-10-19 21:41:00 +0100

    Icecap MVP Version 1.0

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ sys.path.append('../')
 from main.core_platform import research_main
 
 app = celery.Celery('example')
-app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-3-248-43-20.eu-west-1.compute.amazonaws.com:21080')
+app.conf.update(broker_url='rediss://:p10ad8c33e664c24f880e4e617de76589c86a9deb86bd1740b1fd523622ab4883@ec2-52-51-176-162.eu-west-1.compute.amazonaws.com:17730')
 
 @app.task
 def researchworker(upload_data):
