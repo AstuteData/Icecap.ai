baac8cbeee06de860f82c66c7d441813590ce9ae
AstuteData <jack@jackwhitehouse.co.uk>
2023-10-23 21:23:00 +0100

    Icecap MVP Version 1.0

--- flaskapi.py
+++ flaskapi.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tasks
 from main.core_platform.login import loginauth, registration
+from main.return_data import return_researched_companies
 import json
 
 app = Flask(__name__)
@@ -31,6 +32,12 @@ def research():
     upload_data = request.get_json()
     print("Sent to worker")
     response = tasks.researchworker.delay(upload_data)
+
+
+@app.route('/getresearch', methods=['GET'])
+def getresearch():
+    response = return_researched_companies.return_research_list()
+    return response
 
 
 if __name__ == '__main__':
---
+++ main/return_data/return_researched_companies.py
@@ -0,0 +1,29 @@
+import requests
+from sqlalchemy import create_engine, text
+import json
+import pandas as pd
+
+conn = None
+cur = None
+engine = create_engine(
+    'postgresql://xpdmcctztuueoj:5c6b0ce73d0e1d7a8b7ea13688df6b7268edd3e85ddc1ba488a8e233759731d2@ec2-34-241-82-91.'
+    'eu-west-1.compute.amazonaws.com:5432/d6i1k6lrk3j39n')
+
+
+def return_research_list():
+    try:
+        with engine.connect() as conn:
+            company = text('SELECT * FROM "company"')
+            company_data = pd.read_sql_query(company, conn)
+            company_data.drop(columns='index')
+    except Exception as e:
+        print(e)
+
+    researched_company_list = {}
+    for index, row in company_data.iterrows():
+        company_name = row['name']
+        company_id = row['company_id']
+        researched_company_list[company_name] = {'Company Name': company_name,
+                                                 'Company ID': company_id}
+
+    return json.dumps(researched_company_list)
